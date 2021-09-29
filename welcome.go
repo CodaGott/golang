ac47b3b5f9fa80910ef9270e8bec899beea4c0ee
Uyanwune Chidozie Chukwuebuka <talk2don90@gmail.com>
2021-09-29 10:46:53 +0100

    data types and variables with note

--- welcome.go
+++ welcome.go
@@ -4,9 +4,14 @@ import "fmt"
 
 func main()  {
 
-	var name string
-	name = "Dozie"
+	var name string = "Dozie"
+
+	var boolean bool = true
+
+	var number uint = 2
+
+	var floatNum float32 = 2.2
 
 
-	fmt.Println("Hello", name)
+	fmt.Println("Hello", name, boolean, number, floatNum)
 }
